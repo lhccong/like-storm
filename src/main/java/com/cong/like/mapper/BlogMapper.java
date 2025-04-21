f9878ad2d20bc2c6f974d17762a51c0001c9cc6d
lhccong <771901874@qq.com>
2025-04-21 14:49:35 +0800

    feat:新增定时同步点赞数据逻辑

--- src/main/java/com/cong/like/mapper/BlogMapper.java
+++ src/main/java/com/cong/like/mapper/BlogMapper.java
@@ -2,12 +2,16 @@ package com.cong.like.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.cong.like.model.entity.Blog;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Map;
 
 /**
  * @author cong
  */
 public interface BlogMapper extends BaseMapper<Blog> {
 
+    void batchUpdateThumbCount(@Param("countMap") Map<Long, Long> countMap);
 }
 
 
