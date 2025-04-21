b70cc638d96dd52e3230a996bf94ecbd7f224f06
lhccong <771901874@qq.com>
2025-04-21 14:25:28 +0800

    feat:新增 Lua 版点赞校验逻辑

--- src/main/java/com/cong/like/constant/ThumbConstant.java
+++ src/main/java/com/cong/like/constant/ThumbConstant.java
@@ -2,6 +2,10 @@ package com.cong.like.constant;
 
 public interface ThumbConstant {
   
-    String USER_THUMB_KEY_PREFIX = "thumb:";  
-  
+    String USER_THUMB_KEY_PREFIX = "thumb:";
+
+    /**
+     * 临时 点赞记录 key
+     */
+    String TEMP_THUMB_KEY_PREFIX = "thumb:temp:%s";
 }
---
+++ src/main/java/com/cong/like/service/ThumbServiceRedisImpl.java
@@ -0,0 +1,78 @@
+package com.cong.like.service;
+
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.cong.like.common.ErrorCode;
+import com.cong.like.constant.RedisLuaConstant;
+import com.cong.like.excption.BusinessException;
+import com.cong.like.mapper.ThumbMapper;
+import com.cong.like.model.dto.thumb.DoThumbRequest;
+import com.cong.like.model.entity.Thumb;
+import com.cong.like.model.entity.User;
+import com.cong.like.model.enums.LuaStatusEnum;
+import com.cong.like.utils.RedisKeyUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service("thumbService")
+@Slf4j
+@RequiredArgsConstructor
+public class ThumbServiceRedisImpl extends ServiceImpl<ThumbMapper, Thumb> implements ThumbService {
+
+    private final UserService userService;
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Override
+    public Boolean doThumb(DoThumbRequest doThumbRequest, HttpServletRequest request) {
+        if (doThumbRequest == null || doThumbRequest.getBlogId() == null) {
+            throw new BusinessException(ErrorCode.OPERATION_ERROR, "参数错误");
+        }
+        //获取用户、帖子 id
+        User loginUser = userService.getLoginUser(request);
+        Long blogId = doThumbRequest.getBlogId();
+
+        //获取当前整数秒（为了统一处理临时集合）
+        String timeSlice = getTimeSlice();
+        //生成 redis key
+        String tempThumbKey = RedisKeyUtil.getTempThumbKey(timeSlice);
+        String userThumbKey = RedisKeyUtil.getUserThumbKey(loginUser.getId());
+
+        // 执行点赞 Lua 脚本
+        Long result = redisTemplate.execute(
+                RedisLuaConstant.THUMB_SCRIPT,
+                Arrays.asList(tempThumbKey, userThumbKey),
+                loginUser.getId(),
+                blogId
+        );
+
+        if (result == null || LuaStatusEnum.FAIL.getValue() == result) {
+            throw new RuntimeException("用户已点赞");
+        }
+
+        // 更新成功才执行
+        return LuaStatusEnum.SUCCESS.getValue() == result;
+    }
+
+    @Override
+    public Boolean undoThumb(DoThumbRequest doThumbRequest, HttpServletRequest request) {
+        return null;
+    }
+
+    @Override
+    public Boolean hasThumb(Long blogId, Long userId) {
+        return null;
+    }
+
+    private String getTimeSlice() {
+        DateTime nowDate = DateUtil.date();
+        // 获取到当前时间前最近的整数秒，比如当前 11:20:23 ，获取到 11:20:20
+        return DateUtil.format(nowDate, "HH:mm:") + (DateUtil.second(nowDate) / 10) * 10;
+    }
+}
